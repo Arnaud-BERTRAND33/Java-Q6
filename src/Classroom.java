dca01561aa5caa654ed10b0463623d55eef5aac4
Arnaud Bertrand <arnaud.bertrand157@gmail.com>
2022-10-29 08:53:56 +0200

    quete 6

---
+++ src/Classroom.java
@@ -0,0 +1,12 @@
+public class Classroom {
+
+    public static void main(String[] args) {
+        Wilder Loghann = new Wilder("Loghann", true);
+        Wilder Christophe = new Wilder("Christophe", false);
+
+        System.out.println(Loghann.whoAmI());
+        System.out.println(Christophe.whoAmI());
+
+    }
+
+}
